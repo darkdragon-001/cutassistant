2e54caae8fa6d3cdc3b61e4fa1235efa44b1ff3f
a_s_z <a_s_z@01150299-422c-0410-a92d-b503cd675239>
2007-07-26 19:58:29 +0000

    Use zip instead of rar to build zip files.

--- version/makedist.cmd
+++ version/makedist.cmd
@@ -9,16 +9,16 @@ set CA_BIN_ARC=Cut_Assistant_%CA_VER%.ZIP
 svn up %CA_VER%
 svn export %CA_VER% Cut_Assistant_%CA_VER%
 if exist %CA_ARC% del %CA_ARC%
-rar a %CA_ARC% Cut_Assistant_%CA_VER%
+zip -9 -r %CA_ARC% Cut_Assistant_%CA_VER%
 
 cd Cut_Assistant_%CA_VER%
 delphi32 -m cut_assistant.dpr
 upx -9 cut_assistant.exe
-rar a ..\%CA_BIN_ARC% cut_assistant.exe license.txt readme.txt news.txt
+zip -9 ..\%CA_BIN_ARC% cut_assistant.exe license.txt readme.txt news.txt
 
 cd ..
-if exist Cut_Assistant_%CA_VER%.build.zip del Cut_Assistant_%CA_VER%.build.zip
-rar m Cut_Assistant_%CA_VER%.build.zip Cut_Assistant_%CA_VER%
+if exist Cut_Assistant_%CA_VER%.build.rar del Cut_Assistant_%CA_VER%.build.zip
+rar m Cut_Assistant_%CA_VER%.build.rar Cut_Assistant_%CA_VER%
 
 dir *%CA_VER%*
 

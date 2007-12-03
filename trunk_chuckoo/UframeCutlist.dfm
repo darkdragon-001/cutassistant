401900c103c56779f855a329223e583414e599b8
chuckoo <chuckoo@01150299-422c-0410-a92d-b503cd675239>
2007-12-03 11:22:15 +0000

    uframecutlist hinzugefügt, fehlte noch

---
+++ trunk_chuckoo/UframeCutlist.dfm
@@ -0,0 +1,15 @@
+object frameCutlist: TframeCutlist
+  Left = 0
+  Top = 0
+  Width = 320
+  Height = 607
+  TabOrder = 0
+  object Button1: TButton
+    Left = 124
+    Top = 292
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+  end
+end
---
+++ trunk_chuckoo/UframeCutlist.pas
@@ -0,0 +1,25 @@
+unit UframeCutlist;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms, 
+  Dialogs, StdCtrls;
+
+type
+  TframeCutlist = class(TFrame)
+    Button1: TButton;
+  private
+    { Private declarations }
+  public
+
+    { Public declarations }
+  end;
+
+implementation
+
+
+
+{$R *.dfm}
+
+end.
